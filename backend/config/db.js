39b3502d43852abbe2ea6ca2b5dc90f163a81227
rahulpkolhar <rahulkolhar7@gmail.com>
2026-09-03 15:53:34 +0530

    Fix MongoDB connection and CORS for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
-const connectDB=async()=>{
-    try{
-       const con= await mongoose.connect(process.env.MONGO_URI,)
-            
+const mongoose = require("mongoose");
 
-   
-        console.log('mongodb connected successfully');
-    }
-    catch (error){
-        console.error('mongodb connection failed:',error.msg);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
 };
-module.exports=connectDB;
+
+module.exports = connectDB;
